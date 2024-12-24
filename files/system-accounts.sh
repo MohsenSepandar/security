9ab3fff97f229fa884310d7e0ad0ab6c37df11da
root <mohsen.sep@abramad.com>
2024-12-24 12:06:31 +0330

    kolla directory was excluded in log-permission.sh script, all the scripts were changed using bashbeautify command

--- files/system-accounts.sh
+++ files/system-accounts.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 {
- l_output="" l_output2=""
- l_valid_shells="^($( awk -F\/ '$NF != "nologin" {print}' /etc/shells | sed -rn '/^\//{s,/,\\\\/,g;p}' | paste -s -d '|' - ))$"
- a_users=(); a_ulock=() # initialize arrays
- while read -r l_user; do # change system accounts that have a valid login shell to nolog shell
- echo -e " - System account \"$l_user\" has a valid logon shell, changing shell to \"$(which nologin)\""
- usermod -s "$(which nologin)" "$l_user"
- done < <(awk -v pat="$l_valid_shells" -F: '($1!~/(root|sync|shutdown|halt|^\+)/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $(NF) ~ pat) { print $1 }' /etc/passwd)
- while read -r l_ulock; do # Lock system accounts that aren't locked
- echo -e " - System account \"$l_ulock\" is not locked, locking account"
- usermod -L "$l_ulock"
- done < <(awk -v pat="$l_valid_shells" -F: '($1!~/(root|^\+)/ && $2!~/LK?/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $(NF) ~ pat) { print $1 }' /etc/passwd)
+  l_output="" l_output2=""
+  l_valid_shells="^($( awk -F\/ '$NF != "nologin" {print}' /etc/shells | sed -rn '/^\//{s,/,\\\\/,g;p}' | paste -s -d '|' - ))$"
+  a_users=(); a_ulock=() # initialize arrays
+  while read -r l_user; do # change system accounts that have a valid login shell to nolog shell
+    echo -e " - System account \"$l_user\" has a valid logon shell, changing shell to \"$(which nologin)\""
+    usermod -s "$(which nologin)" "$l_user"
+  done < <(awk -v pat="$l_valid_shells" -F: '($1!~/(root|sync|shutdown|halt|^\+)/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $(NF) ~ pat) { print $1 }' /etc/passwd)
+  while read -r l_ulock; do # Lock system accounts that aren't locked
+    echo -e " - System account \"$l_ulock\" is not locked, locking account"
+    usermod -L "$l_ulock"
+  done < <(awk -v pat="$l_valid_shells" -F: '($1!~/(root|^\+)/ && $2!~/LK?/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $(NF) ~ pat) { print $1 }' /etc/passwd)
 }
 
--- files/user-default-parameters.sh
+++ files/user-default-parameters.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cat /etc/passwd | while read -r line; do 
-	USER_ID=$(echo $line | cut -d : -f 3);
-	USER_NAME=$(echo $line | cut -d : -f 1)
-	if [[ $USER_ID -gt 1000 && $USER_ID -le 65530 ]];then
-		chage --mindays 1 $USER_NAME
-		chage --maxdays 365 $USER_NAME
-		chage --warndays 7 $USER_NAME
-		chage --inactive 30 $USER_NAME
-	fi
+cat /etc/passwd | while read -r line; do
+  USER_ID=$(echo $line | cut -d : -f 3);
+  USER_NAME=$(echo $line | cut -d : -f 1)
+  if [[ $USER_ID -gt 1000 && $USER_ID -le 65530 ]];then
+    chage --mindays 1 $USER_NAME
+    chage --maxdays 365 $USER_NAME
+    chage --warndays 7 $USER_NAME
+    chage --inactive 30 $USER_NAME
+  fi
 done
