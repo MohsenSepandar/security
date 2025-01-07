64a9a38a46f0ce2f1fa0e05758cef7e01f140f74
root <mohsen.sep@abramad.com>
2025-01-07 10:16:46 +0330

    adding ssh.yml task file, modifying the chrony.yml file and changing parameters in user-accounts.yml

--- files/user-default-parameters.sh
+++ files/user-default-parameters.sh
@@ -5,7 +5,7 @@ cat /etc/passwd | while read -r line; do
   USER_NAME=$(echo $line | cut -d : -f 1)
   if [[ $USER_ID -gt 1000 && $USER_ID -le 65530 ]];then
     chage --mindays 1 $USER_NAME
-    chage --maxdays 365 $USER_NAME
+    chage --maxdays 180 $USER_NAME
     chage --warndays 7 $USER_NAME
     chage --inactive 30 $USER_NAME
   fi
